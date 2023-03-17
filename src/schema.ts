11a1a3178b1413ff8676b97beb7f6f0a7f225fe0
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2023-03-17 18:30:42 -0500

    Clean up imports

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,8 @@
 import { builder } from "./builder";
-import { prisma } from "./db";
 
 import "./models/Goal";
 import "./models/GroupLesson";
 import "./models/Lesson";
-import { Lesson, LessonInput, LessonInputPartial } from "./models/Lesson"
 import "./models/Rating";
 import "./models/School";
 import "./models/Student";
