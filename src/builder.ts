3a7c33155c022154b469541c23df68dfc6053fd3
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2023-03-17 01:49:00 -0500

    Set type of ID to BigInt

--- src/builder.ts
+++ src/builder.ts
@@ -1,5 +1,5 @@
 import SchemaBuilder from "@pothos/core";
-import { BigIntResolver, DateTimeResolver, NonEmptyStringResolver } from "graphql-scalars";
+import { BigIntResolver, DateTimeResolver } from "graphql-scalars";
 import PrismaPlugin from "@pothos/plugin-prisma";
 import type PrismaTypes from "@pothos/plugin-prisma/generated";
 import { prisma } from "./db";
@@ -7,7 +7,7 @@ import { prisma } from "./db";
 export const builder = new SchemaBuilder<{
   Scalars: {
     DateTime: { Input: Date; Output: Date };
-    //ID: { Input: BigInt; Output: BigInt };
+    ID: { Input: BigInt; Output: BigInt };
   };
   PrismaTypes: PrismaTypes;
 }>({
@@ -17,11 +17,6 @@ export const builder = new SchemaBuilder<{
   },
 });
 
-//builder.addScalarType("ID", NonEmptyStringResolver, { serialize: ((x: any) => x.id.toString()) });
-//builder.addScalarType("ID", BigIntResolver, {});
-/*builder.scalarType("ID", {
-  serialize: (n) => n.toString(),
-  parseValue: (n: any) => n.toString() //BigInt(n as (string | number | bigint | boolean))
-});*/
 builder.addScalarType("DateTime", DateTimeResolver, {});
+builder.addScalarType("ID", BigIntResolver, {});
 
